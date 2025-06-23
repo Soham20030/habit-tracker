c095b68eebddb31745116189f1c4569c4d4d1859
Soham Kotkar <soham20090@gmail.com>
2025-06-23 13:46:01 -0700

    added redirect file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ✅ Modified config to support React Router deployment
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
   }
 })
