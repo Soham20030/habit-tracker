d0bdb4beb9f12122de2e4e7e9ed644ed47b43db9
Soham Kotkar <soham20090@gmail.com>
2025-06-23 12:07:58 -0700

    fix: update CORS config to allow localhost and deployed frontend

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true
+  origin: [
+    "https://habit-tracker-8eqs.onrender.com", // deployed frontend
+    "http://localhost:5173"                    // local frontend dev
+  ],
+  credentials: true
 }));
 
 // Add these middleware lines:
